7e315b8fd69fd47a082080a04d04e170f92f1756
Amareteklay <amareteklay@gmail.com>
2025-08-22 15:55:35 +0200

    EOD: removed unused files

--- src/epps_shocks/utils.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import pandas as pd
-
-def save_csv(df: pd.DataFrame, path: Path, index: bool=False) -> Path:
-    path.parent.mkdir(parents=True, exist_ok=True)
-    df.to_csv(path, index=index)
-    return path
